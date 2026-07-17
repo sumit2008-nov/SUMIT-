3b1220b0fc89df0981e1fe6689ef4d5440d7f10b
sumit2008-nov <sumitkumar456kr@gmail.com>
2026-07-17 23:01:28 +0530

    inital unity project

---
+++ Assets/BallMovement.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    public float speed = 10f;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveX, 0f, moveZ);
+        rb.AddForce(movement * speed);
+    }
+}
